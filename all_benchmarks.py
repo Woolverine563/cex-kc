8a88c7dddb9a48f8765576039a2e5940361d3359
Sahil Jain <jsahil730@gmail.com>
2022-03-09 02:57:29 +0530

    all benchmarks

---
+++ all_benchmarks.py
@@ -0,0 +1,16 @@
+import os
+
+f = open('benchmarks_to_run', 'w')
+
+for a in os.listdir('benchmarks'):
+    if os.path.isdir(f'benchmarks/{a}'):
+        b = f'benchmarks/{a}'
+        c = ''
+        if (os.path.exists(f'{b}/verilog')):
+            c = f'{b}/verilog'
+        else:
+            c = f'{b}/aiger'
+        for x in os.listdir(c):
+            if os.path.isfile(f'{c}/{x}'):
+                f.write(f'{c}/{x}\n')
+f.close()
